7c7375adbed01d39f3d8a91000b6252816b59612
venom157 <sander_walstock@hotmail.com>
2013-03-25 19:30:46 +0100

    Comments added

--- linerider/Main.js
+++ linerider/Main.js
@@ -1,3 +1,6 @@
+/*
+ * Author : Sander Walstock
+ */
 var box2d = {
 	b2Vec2 : Box2D.Common.Math.b2Vec2,
 	b2BodyDef : Box2D.Dynamics.b2BodyDef,
@@ -11,7 +14,7 @@ var box2d = {
 	b2DebugDraw : Box2D.Dynamics.b2DebugDraw
 };
 
-
+//globals
 var canvasm, canvaso, canvasd;
 var b;
 var ctx, ctx2, ctx3;
@@ -24,12 +27,9 @@ var tool_default = 'pencil';
 // This object holds the implementation of each drawing tool.
 var tools = {};
 
-//radiaal naar graden conversie
-var rad_to_deg = 180/Math.PI;
-
 function e_stage(e) {
-    e._x = e.stageX;
-    e._y = e.stageY;
+    e._x = e.stageX; //event coordinates on stage
+    e._y = e.stageY; //without stage offset
 
   // Call the event handler of the tool.
   var func = tool[e.type];
@@ -38,54 +38,57 @@ function e_stage(e) {
   }
 }
 
+//event handler for toolselect
 function e_tool_change(e){
     if(tools[this.value]){
         tool = new tools[this.value]();
     }
 }
 
+//pencil tool
 tools.pencil = function(){
     var tool = this;
     this.started = false;
     
-    this.stagemousedown = function(e){
-        tool.x0 = e._x;
+    this.stagemousedown = function(e){ //movedown eventhandler
+        tool.x0 = e._x; //initial pen coordinates get stored in tool object
         tool.y0 = e._y;
-        tool.started = true;
+        tool.started = true; //started drawing
         drawobj.graphics
                      .setStrokeStyle(2, "square")
-                     .beginStroke("#000")
-                     .moveTo((tool.x0 - stage.x), (tool.y0 - stage.y));
+                     .beginStroke("#000") //color
+                     .moveTo((tool.x0 - stage.x), (tool.y0 - stage.y)); //pen movement including stage offset
     };
 
     this.stagemousemove = function(e){
         tool.xf = e._x;
         tool.yf = e._y;
-        tool.xw = tool.xf - tool.x0;
-        tool.yw = tool.yf - tool.y0;
+        tool.xw = tool.xf - tool.x0; //length of the line in x
+        tool.yw = tool.yf - tool.y0; //length of the line in y
         if (tool.started) {
-            if (Math.sqrt(tool.xw * tool.xw + tool.yw * tool.yw) > 10) {
-                var lineBodyDef = new box2d.b2BodyDef;
-                lineBodyDef.type = box2d.b2Body.b2_staticBody;
-                lineBodyDef.position.x = (tool.x0 - stage.x) / SCALE;
+            if (Math.sqrt(tool.xw * tool.xw + tool.yw * tool.yw) > 10) { //simple pythagoras for checking that line length is longer then .. pixels
+                var lineBodyDef = new box2d.b2BodyDef; //for more info check
+                lineBodyDef.type = box2d.b2Body.b2_staticBody; //Box2dweb lib
+                lineBodyDef.position.x = (tool.x0 - stage.x) / SCALE;//body position
                 lineBodyDef.position.y = (tool.y0 - stage.y) / SCALE;
 
-                var lineFixtureDef = new box2d.b2FixtureDef;
+                var lineFixtureDef = new box2d.b2FixtureDef; //fixdef
                 //lineFixtureDef.filter.groupIndex = -1;
                 lineFixtureDef.shape = new box2d.b2PolygonShape;
-                lineFixtureDef.shape.SetAsEdge(new box2d.b2Vec2(0, 0), new box2d.b2Vec2(tool.xw / SCALE, tool.yw / SCALE));
-                lineFixtureDef.friction = 0.1;
-                world.CreateBody(lineBodyDef).CreateFixture(lineFixtureDef);
-
-                drawobj.graphics.lineTo((tool.xf - stage.x), (tool.yf - stage.y));
-                tool.x0 = tool.xf;
+                lineFixtureDef.shape.SetAsEdge(new box2d.b2Vec2(0, 0), new box2d.b2Vec2(tool.xw / SCALE, tool.yw / SCALE)); //setting it as an edge for thin lines, with x,y values
+                lineFixtureDef.friction = 0.1; //friction a value between 0-1
+                world.CreateBody(lineBodyDef).CreateFixture(lineFixtureDef); //should try something else here:
+				//drawobj.body = world.CreateBody(lineBodyDef); //not tested
+				//drawobj.body.CreateFixture(lineFixtureDef);
+                drawobj.graphics.lineTo((tool.xf - stage.x), (tool.yf - stage.y));//draw line with pen to given x,y
+                tool.x0 = tool.xf; //final point of the .. pixels saved
                 tool.y0 = tool.yf;
             }
                 
         }
     };
 
-    this.stagemouseup = function(){
+    this.stagemouseup = function(){ //obvious
         if(tool.started){
             tool.started = false;
         }
@@ -112,7 +115,7 @@ tools.line = function(){
                     .setStrokeStyle(2, "square")
                     .beginStroke("#cbc53d")
                     .moveTo((tool.x0 - stage.x),(tool.y0 - stage.y))
-                    .lineTo((tool.xf - stage.x),(tool.yf - stage.y));
+                    .lineTo((tool.xf - stage.x),(tool.yf - stage.y));//straight line is drawn on the overlay stage first and is cleared everytime you move.
         }
     };
 
@@ -130,13 +133,13 @@ tools.line = function(){
             lineFixtureDef.shape = new box2d.b2PolygonShape;
             lineFixtureDef.shape.SetAsEdge(new box2d.b2Vec2(0, 0), new box2d.b2Vec2(tool.xw / SCALE, tool.yw / SCALE));
             lineFixtureDef.friction = 0.1;
-            world.CreateBody(lineBodyDef).CreateFixture(lineFixtureDef);
+            world.CreateBody(lineBodyDef).CreateFixture(lineFixtureDef);//should also try something else here!
 
             drawobj.graphics
                     .setStrokeStyle(2, "square")
                     .beginStroke("#000")
                     .moveTo((tool.x0 - stage.x), (tool.y0 - stage.y))
-                    .lineTo((tool.xf - stage.x), (tool.yf - stage.y));
+                    .lineTo((tool.xf - stage.x), (tool.yf - stage.y));//line is now drawn on the mainstage
             lineobj.graphics.clear();
             tool.started = false;
         }
@@ -152,7 +155,7 @@ tools.drag = function () {
         tool.y0 = e._y;
         tool.started = true;
     };
-
+	//this moves all the stages so that you can drag. Just simple math
     this.stagemousemove = function (e) {
         tool.xf = e._x;
         tool.yf = e._y;
@@ -180,15 +183,15 @@ tools.drag = function () {
 function init() {
     canvasm = document.getElementById("canvas");
     canvaso = document.getElementById("overlaycanvas");
-    canvasd = document.getElementById("debugcanvas");
+    //canvasd = document.getElementById("debugcanvas");//debugcanvas is not used right now
     button_start = document.getElementById("startbutton");
     ctx = canvasm.getContext('2d');
     ctx2 = canvaso.getContext('2d');
-    ctx3 = canvasd.getContext('2d');
+    //ctx3 = canvasd.getContext('2d');
 
-    stage = new createjs.Stage(canvasm);
+    stage = new createjs.Stage(canvasm); //creating easeljs stages
     overlaystage = new createjs.Stage(canvaso);
-    world = new box2d.b2World(new box2d.b2Vec2(0, 9.81), true);
+    world = new box2d.b2World(new box2d.b2Vec2(0, 9.81), true); //setting up box2d world for physics
     
     //tool selection
     var tool_select = document.getElementById('dtool');
@@ -196,7 +199,7 @@ function init() {
         alert("Error: No tool selected");
         return;
     }
-    tool_select.addEventListener('change', e_tool_change, false);
+    tool_select.addEventListener('change', e_tool_change, false);//event listeners for tool and button
     button_start.addEventListener('click', newball, false);
     
     if (tools[tool_default]) {
@@ -204,34 +207,34 @@ function init() {
         tool_select.value = tool_default;
     }
 
-    drawobj = new createjs.Shape();
+    drawobj = new createjs.Shape(); //adding line shape
     lineobj = new createjs.Shape();
     
     /*canvasd.onmousedown = e_stage;
     canvasd.onmousemove = e_stage;
     canvasd.onmouseup = e_stage;*/
 
-    overlaystage.onMouseDown = e_stage;
+    overlaystage.onMouseDown = e_stage;//event handlers on stage
     overlaystage.onMouseMove = e_stage;
     overlaystage.onMouseUp = e_stage;
 
-    stage.addChild(drawobj);
+    stage.addChild(drawobj);//adding line objects
     overlaystage.addChild(lineobj);
 
-    createjs.Ticker.addListener(this);
-    createjs.Ticker.setFPS(100);
-    createjs.Ticker.useRAF = true;
+    createjs.Ticker.addListener(this);//use default tick
+    createjs.Ticker.setFPS(100); //100fps other settings feel slow
+    createjs.Ticker.useRAF = true;//forgot what it meant
 }
 
 
-function newball(e) {
+function newball(e) { //eventhandler for adding a new ball
     b = new Ball();
     stage.addChild(b.view);
 }
 
-function tick(e) {
+function tick(e) { //tick event updating the world
     stage.update();
     overlaystage.update();
-	world.Step(1/50, 5, 5);
-	world.ClearForces();;
+	world.Step(1/50, 5, 5); //best setting in my opinion otherwise world feels slow. Perhaps more calculations per step needed
+	world.ClearForces();
 }
